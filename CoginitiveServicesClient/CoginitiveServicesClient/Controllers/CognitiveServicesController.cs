c99c8e42e45afc93fc18631a4a98aba6b76e1cb9
Mario Estuardo Gómez Orellana <mario.e.g.orellana@gmail.com>
2021-10-10 19:39:41 -0600

    Project creation

---
+++ CoginitiveServicesClient/CoginitiveServicesClient/Controllers/CognitiveServicesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoginitiveServicesClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CognitiveServicesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("File upload running API");
+        }
+    }
+}
